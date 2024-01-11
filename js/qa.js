c95152a63d8ae561f75c6e9ad767c0ad79220c50
Halina Tulchii <halendeihalyna@gmail.com>
2024-01-11 14:34:06 +0200

    added questions and answers to qa page

---
+++ js/qa.js
@@ -0,0 +1,9 @@
+console.log('hello acord')
+
+const headers = document.querySelectorAll('[data-name="accordeon-title"]');
+
+headers.forEach(function (item) {
+    item.addEventListener('click', function () {
+        this.nextElementSibling.classList.toggle('hidden');
+    })
+})
